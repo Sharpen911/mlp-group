cf6f9afa3e2fbb2cb2e4169c131e25e84ba13cf2
Sharpen <635023135@qq.com>
2020-03-03 20:04:38 +0000

    Add data and code

---
+++ prepare_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+import random
+
+
+
+
+dataset_name = 'StackOverflow'
+with open('StackOverflow.txt', 'r',encoding='utf-8') as f:
+    sentences = f.read().splitlines()
+with open('StackOverflow_gnd.txt', 'r',encoding='utf-8') as f:
+    labels = f.read().splitlines()
+random.seed(233333)
+random.shuffle(sentences)
+random.seed(233333)
+random.shuffle(labels)
+
+
+train_list = ['train' for i in range(int(0.7*len(sentences)))]
+test_list = ['test' for j in range(int(0.3*len(sentences)))]
+train_or_test_list = train_list+test_list
+
+
+
+meta_data_list = []
+
+for i in range(len(sentences)):
+    meta = str(i) + '\t' + train_or_test_list[i] + '\t' + labels[i]
+    meta_data_list.append(meta)
+
+meta_data_str = '\n'.join(meta_data_list)
+
+f = open('data/' + dataset_name + '.txt', 'w',encoding='utf-8')
+f.write(meta_data_str)
+f.close()
+
+corpus_str = '\n'.join(sentences)
+
+f = open('data/corpus/' + dataset_name + '.txt', 'w',encoding='utf-8')
+f.write(corpus_str)
+f.close()
---
+++ tool.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+with open('clickbait.txt', 'r',encoding='utf-8') as f:#load the clickbait title
+    lines = f.readlines()
+clickbait=[]
+for line in lines:
+    if line != "\n":
+        clickbait.append(line.strip())
+
+with open('non_clickbait.txt', 'r',encoding='utf-8') as f:#load the non_clickbait title
+    lines = f.readlines()
+non_clickbait=[]
+for line in lines:
+    if line != "\n":
+        non_clickbait.append(line.strip())
+
+labels=[]
+clickbait_train_sentences=clickbait[:int(0.7*len(clickbait))]
+labels+=['1' for i in range(len(clickbait_train_sentences))]#add labels for clickbait type
+
+non_clickbait_train_sentences=non_clickbait[:int(0.7*len(non_clickbait))]
+labels+=['0' for i in range(len(non_clickbait_train_sentences))]#add labels for non_clickbait type
+
+clickbait_test_sentences = clickbait[int(0.7*len(clickbait)):]
+labels+=['1' for i in range(len(clickbait_test_sentences))]
+
+non_clickbait_test_sentences = non_clickbait[int(0.7*len(non_clickbait)):]
+labels+=['0' for i in range(len(non_clickbait_test_sentences))]
+
+train_or_test_list = []
+
+train_sentences = clickbait_train_sentences+non_clickbait_train_sentences
+train_or_test_list+=['train' for i in range(len(train_sentences))]
+
+test_sentences = clickbait_test_sentences+non_clickbait_test_sentences
+train_or_test_list+=['test' for i in range(len(test_sentences))]
+
+sentences = train_sentences+test_sentences
+
+
+
+
+'''
+build my own dataset
+'''
+dataset_name = 'clickbait'
+meta_data_list = []
+
+for i in range(len(sentences)):
+    meta = str(i) + '\t' + train_or_test_list[i] + '\t' + labels[i]
+    meta_data_list.append(meta)
+
+meta_data_str = '\n'.join(meta_data_list)
+
+f = open('data/' + dataset_name + '.txt', 'w',encoding='utf-8')
+f.write(meta_data_str)
+f.close()
+
+corpus_str = '\n'.join(sentences)
+
+f = open('data/corpus/' + dataset_name + '.txt', 'w',encoding='utf-8')
+f.write(corpus_str)
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
